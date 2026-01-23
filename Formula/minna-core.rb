5e451bae0072830fdcdc21e9a3d14d084142a7e3
William Pong <hi@wllmp.com>
2026-01-23 12:40:29 -0800

    Fix sha256 for v0.2.0 release

--- Formula/minna-core.rb
+++ Formula/minna-core.rb
@@ -2,7 +2,7 @@ class MinnaCore < Formula
   desc "Your AI's memory. Local-first. Zero config."
   homepage "https://github.com/getminna/minna-core"
   url "https://github.com/getminna/minna-core/releases/download/v0.2.0/minna-0.2.0-aarch64-apple-darwin.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "81774c7a7ac836832073ef88aa9afd0dd25b2e7c683b1b22cd3c1f8a95c01f9c"
   version "0.2.0"
 
   depends_on arch: :arm64
